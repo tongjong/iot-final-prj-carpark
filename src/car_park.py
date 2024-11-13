fb4ee81e2b9da02c05ddb6847930bf09475a2338
J155917 <J155917@tafe.wa.edu.au>
2024-11-13 14:47:51 +0800

    feat: Add methods to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -14,6 +14,19 @@ class CarPark:
     def get_car_park(cls, name, capacity, location, display):
         return cls(name, capacity, location, display)
 
+    @property
+    def car_park_info(self) -> str:
+        return f"Welcome to {self.name} in {self.location}!  It is ready to operate."
 
+    @property
+    def available_bays(self) -> int:
+        return max(self.capacity - len(self.plates), 0)
+
+    def update_display(self, message=None) -> None:
+        self.display.available_bays = self.available_bays
+        self.display.temperature = random.randint(10,40)
+        if message is not None:
+            self.display.message = message
+        self.display.show()
 
 
