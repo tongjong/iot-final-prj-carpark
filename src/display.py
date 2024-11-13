bb66357454896b30777ef7582bbabf386e60a9c8
J155917 <J155917@tafe.wa.edu.au>
2024-11-13 18:01:07 +0800

    fix: community message display layout logic

--- src/display.py
+++ src/display.py
@@ -12,16 +12,23 @@ class Display:
 
     def show_update(self, available_bays: int, temperature: float, message: str) -> str:
         if self.display_mode == DisplayMode.PLAIN:
-            return f'Available Bays: {'FULL' if available_bays == 0 else available_bays}\n Temperature: {temperature}\n Message: {message}'
+            return f'\nAvailable Bays: {'FULL' if available_bays == 0 else available_bays}\nTemperature: {temperature}\nMessage: {message}\n'
         elif self.display_mode == DisplayMode.STYLE:
-            display_message =  f'--------------------------------------------------\n' \
+            display_message =  f'\n--------------------------------------------------\n' \
                                 f'--------------Available Bays: {'FULL' if available_bays == 0 else available_bays}-----------------\n' \
                                 '--------------------------------------------------\n' \
                                 f'----------------Temperature: {temperature}-------------------\n' \
                                 f'--------------------------------------------------\n' \
                                 f'💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮\n'
             line_length = 40
-            for i in range(0, len(message), line_length):
-                display_message += message[i:i + line_length] + '\n'
+            words = message.split(' ')
+            line = ''
+            while len(words) > 0:
+                if len(line) < line_length:
+                    line += words.pop(0) + ' '
+                else:
+                    display_message += line + '\n'
+                    line = ''
+            display_message += line + '\n'
 
             return display_message
