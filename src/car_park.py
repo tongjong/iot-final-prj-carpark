e5953d099065123ebcfefcbfef6732a1add94fb5
J155917 <J155917@tafe.wa.edu.au>
2024-11-13 13:08:25 +0800

    feat: add a constructor and attributes to the car park class

---
+++ src/car_park.py
@@ -0,0 +1,11 @@
+from display import Display
+
+class CarPark:
+    def __init__(self, capacity: int, location: str, display: Display):
+        self.capacity = capacity
+        self.location = location
+        self.display = display
+        self.plates = []
+
+
+
