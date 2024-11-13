617f5384ae6ae232de51eb3354297064f363bcc2
J155917 <J155917@tafe.wa.edu.au>
2024-11-13 14:43:18 +0800

    feat: Add a second option for creating car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,11 +1,19 @@
+import random
+
 from display import Display
 
 class CarPark:
-    def __init__(self, capacity: int, location: str, display: Display):
+    def __init__(self,name:str, capacity: int, location: str, display: Display):
+        self.name = name
         self.capacity = capacity
         self.location = location
         self.display = display
         self.plates = []
+
+    @classmethod
+    def get_car_park(cls, name, capacity, location, display):
+        return cls(name, capacity, location, display)
+
 
 
 
